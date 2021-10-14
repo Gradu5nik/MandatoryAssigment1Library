1fcc353fac7fd2bc26b8a2ae6d19901d8cdc7592
george <geor2022@edu.easj.dk>
2021-10-14 10:45:33 +0200

    BookClass

---
+++ MandatoryAssigment1Library/Book.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MandatoryAssigment1Library
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int NoOfPages { get; set; }
+        public string ISBN13 { get; set; }
+    }
+}
